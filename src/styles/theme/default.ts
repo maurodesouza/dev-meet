21756242cbe180c777526b52b34945252aee14dd
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-03-03 01:29:09 -0300

    ⚡  chore: add more font configs to default theme

--- src/styles/theme/default.ts
+++ src/styles/theme/default.ts
@@ -10,18 +10,33 @@ const theme = {
     transparent: 'transparent'
   },
 
-  fonts: {
-    primary: {
-      regular: 'Epilogue_400Regular',
-      medium: 'Epilogue_500Medium',
-      semi_bold: 'Epilogue_600SemiBold',
-      bold: 'Epilogue_700Bold',
-      extra_bold: 'Epilogue_800ExtraBold',
+  font: {
+    family: {
+      primary: {
+        regular: 'Epilogue_400Regular',
+        medium: 'Epilogue_500Medium',
+        semi_bold: 'Epilogue_600SemiBold',
+        bold: 'Epilogue_700Bold',
+        extra_bold: 'Epilogue_800ExtraBold',
+      },
+
+      secondary: {
+        regular: 'Rajdhani_400Regular',
+        medium: 'Rajdhani_500Medium',
+      }
     },
 
-    secondary: {
-      regular: 'Rajdhani_400Regular',
-      medium: 'Rajdhani_500Medium',
+    sizes: {
+      xxxsmall: 8,
+      xxsmall: 10,
+      xsmall: 12,
+      small: 14,
+      medium: 16,
+      large: 20,
+      xlarge: 24,
+      xxlarge: 28,
+      xxxlarge: 32,
+      xxxxlarge: 40,
     }
   },
 }
