c6f979982385f3de5702dfc66d37a28f5a006e47
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-03-02 23:36:37 -0300

    ⚡  chore: add theme

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
+import { ThemeProvider } from 'styled-components'
 
+import { theme } from './src/styles'
 import { StackNavigation } from './src/navigation'
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <StackNavigation />
       <StatusBar style="auto" />
-    </>
+    </ThemeProvider>
   );
 }
---
+++ src/styles/theme/default.ts
@@ -0,0 +1,13 @@
+const theme = {
+  colors: {
+    primary: '#FF5100',
+    secondary: '#04D361',
+    bg: '#282828',
+    white: '#FFFFFF',
+    gray: '#C6C6C6',
+    gray300: '#BFBFBF',
+    gray400: '#959595',
+  }
+}
+
+export { theme }
