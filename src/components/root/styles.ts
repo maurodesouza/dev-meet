6750f2ecdcd611a8509a3866d26a304009d88d55
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-03-04 02:28:30 -0300

    🐛  fix: root styles

--- src/components/root/styles.ts
+++ src/components/root/styles.ts
@@ -2,7 +2,8 @@ import styled, { css } from 'styled-components/native'
 
 export const Container = styled.View`
   ${({ theme }) => css`
+    padding: ${theme.spacings.xxxlarge}px ${theme.spacings.large}px 0;
     background: ${theme.colors.bg};
-    flex: 1;
+    height: 100%;
   `}
 `
