da53e26bc5b62a039e619e807ad741bc8758cd8e
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-03-04 02:28:13 -0300

    ⚡  chore: add dashboard elements

--- src/screens/dashboard/index.tsx
+++ src/screens/dashboard/index.tsx
@@ -1,10 +1,25 @@
-import { Text } from '../../components'
+import { EventTypeCard } from '../../components'
+import { useFetch } from '../../hooks'
+
+import { Type } from '../../types'
 import * as S from './styles'
 
 const Dashboard = () => {
+  const { data } = useFetch()
+
   return (
     <S.Container>
-      <Text>Dashboard</Text>
+        <S.Header>Que tipo de evento você procura?</S.Header>
+        <S.Description>Selecione a categoria que{'\n'}mais te agrada!</S.Description>
+
+        <S.Contents
+          data={data?.types}
+          keyExtractor={item => String((item as Type).id)}
+          numColumns={2}
+          renderItem={({ item  }) => (
+            <EventTypeCard {...item as Type} />
+          )}
+        />
     </S.Container>
   )
 }
--- src/screens/dashboard/styles.ts
+++ src/screens/dashboard/styles.ts
@@ -1,5 +1,28 @@
-import styled from 'styled-components/native'
+import styled, { css } from 'styled-components/native'
+import { Text } from '../../components'
 
 export const Container = styled.View`
   flex: 1;
+`
+
+export const Header = styled(Text).attrs(() => ({
+  size: 'xxxlarge',
+  color: 'white',
+  weight: 'bold',
+}))`
+  ${({ theme }) => css`
+    margin-bottom: ${theme.spacings.medium}px;
+  `}
+`
+
+export const Description = styled(Text).attrs(() => ({
+  size: 'large',
+}))`
+  ${({ theme }) => css`
+    margin-bottom: ${theme.spacings.large}px;
+  `}
+`
+
+export const Contents = styled.FlatList`
+  flex-shrink: 0;
 `
