db4a71811c1af1f89cdabdff50a1446848abe44d
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-03-04 04:31:58 -0300

    ✨  feat: add events screen

--- src/screens/events/index.tsx
+++ src/screens/events/index.tsx
@@ -1,10 +1,29 @@
-import { Text } from '../../components'
+import { Header, EventCard } from '../../components'
+import { useFetch } from '../../hooks'
+
+import { Event } from '../../types'
 import * as S from './styles'
 
 const Events = () => {
+  const { data } = useFetch()
+
   return (
     <S.Container>
-      <Text>Events</Text>
+      <Header>
+        <S.Title>Eventos {'\n'}disponíveis</S.Title>
+      </Header>
+
+      <S.Description>
+        Selecione o evento desejado.{'\n'}E espere sua data!
+      </S.Description>
+
+      <S.Contents
+        data={data?.events}
+        keyExtractor={item => (item as Event).id}
+        renderItem={({ item  }) => (
+          <EventCard {...item as Event} />
+        )}
+      />
     </S.Container>
   )
 }
--- src/screens/events/styles.ts
+++ src/screens/events/styles.ts
@@ -1,5 +1,25 @@
-import styled from 'styled-components/native'
+import styled, { css } from 'styled-components/native'
+import { Text } from '../../components'
 
 export const Container = styled.View`
   flex: 1;
+`
+
+export const Title = styled(Text).attrs(() => ({
+  size: 'xxxlarge',
+  color: 'white',
+  weight: 'bold',
+}))``
+
+export const Description = styled(Text).attrs(() => ({
+  size: 'medium',
+}))`
+  ${({ theme }) => css`
+    margin-top: ${theme.spacings.small}px;
+    margin-bottom: ${theme.spacings.large}px;
+  `}
+`
+
+export const Contents = styled.FlatList`
+  flex-shrink: 0;
 `
