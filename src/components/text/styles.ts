3f9ce58aa9db89e04c4c3eb05fc9158610d4af32
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-03-04 02:23:17 -0300

    🐛  fix: text component size

--- src/components/text/styles.ts
+++ src/components/text/styles.ts
@@ -5,7 +5,6 @@ type ContainerProps = Required<Omit<TextProps, 'children'>>
 
 export const Container = styled.Text<ContainerProps>`
   ${({ theme, color, family, weight, size, bg, lineHeight }) => css`
-    flex: 1;
     color: ${theme.colors[color]};
     background: ${theme.colors[bg]};
     font-family: ${(theme.font.family as any)[family][weight]};
