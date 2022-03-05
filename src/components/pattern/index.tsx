76886cabc87831aae9104bc15f3c28c6fa2c8037
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-03-05 03:58:20 -0300

    🐛  fix: add key in map render

--- src/components/pattern/index.tsx
+++ src/components/pattern/index.tsx
@@ -5,8 +5,8 @@ import * as S from './styles'
 const Pattern = () => {
   return (
     <S.Container>
-      {pattern.map(({ children, gap, spacing, ...props}) => (
-        <S.Wrapper gap={gap} spacing={spacing}>
+      {pattern.map(({ children, gap, spacing, ...props}, index) => (
+        <S.Wrapper key={index} gap={gap} spacing={spacing}>
           <S.Shimmer  {...props} />
           {children && (
             <>
