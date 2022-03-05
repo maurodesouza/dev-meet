961158be42871f0238f2be8bdbdd64469d978a38
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-03-05 00:56:41 -0300

    ⚡  chore: add next component

---
+++ src/components/next/index.tsx
@@ -0,0 +1,29 @@
+import { DefaultTheme } from 'styled-components'
+import { TouchableOpacityProps } from 'react-native'
+
+import * as S from './styles'
+
+type NextProps = TouchableOpacityProps & {
+  showLabel?: boolean
+  arrowColor?: keyof DefaultTheme['colors']
+  arrowBg?: keyof DefaultTheme['colors']
+  iconSize?: number
+}
+
+const Next = ({ showLabel = false, arrowBg = 'primary', arrowColor = 'white', iconSize = 48, ...rest }: NextProps) => {
+  return (
+    <S.Container {...rest}>
+      {showLabel && <S.Label>Próximo</S.Label>}
+
+      <S.Wrapper bg={arrowBg} size={iconSize}>
+        <S.Icon
+          name="arrow-right"
+          color={arrowColor}
+          size={iconSize / 2}
+        />
+      </S.Wrapper>
+    </S.Container>
+  )
+}
+
+export { Next }
---
+++ src/components/next/styles.ts
@@ -0,0 +1,41 @@
+import styled, { css, DefaultTheme } from 'styled-components/native'
+import { Feather } from "@expo/vector-icons";
+
+import { Text } from '../../components'
+
+export const Container = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+`
+
+export const Label = styled(Text).attrs(() => ({
+  size: 'medium',
+  color: 'white',
+  weight: 'semi_bold',
+}))`
+  ${({ theme }) => css`
+    margin-right: ${theme.spacings.xsmall}px;
+  `}
+`
+
+type WrapperProps = {
+  size: number;
+  bg: keyof DefaultTheme['colors'];
+}
+
+export const Wrapper = styled.View<WrapperProps>`
+  ${({ theme, size, bg }) => css`
+    background: ${theme.colors[bg]};
+    border-radius: 10px;
+    height: ${size}px;
+    width: ${size}px;
+    padding: ${theme.spacings.xxsmall}px;
+    align-items: center;
+    justify-content: center;
+  `}
+`
+
+export const Icon = styled(Feather).attrs(({ theme }) => ({
+  color: theme.colors.white
+}))``
+
